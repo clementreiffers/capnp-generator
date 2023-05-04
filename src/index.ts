d3ab5517c9fbedbd61746083831d65dc28d9628b
clementreiffers <reiffersclement@gmail.com>
2023-05-04 15:51:49 +0200

    correction où des espaces manquaient mais pbs de noms

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {
 	type ModuleList,
 	type WorkerdModule,
 } from './types';
+import {writeFile} from 'fs';
 /*
 
 What do we need ?
@@ -22,7 +23,7 @@ const workerdModuleFactory = (name: string, file: string, compatibilityDate: Com
 	compatibilityDate,
 });
 
-const moduleCapnpify = (module: Module): string => `(name = ${module.name}, esModule = embed ${module.esModule})`;
+const moduleCapnpify = (module: Module): string => `(name = "${module.name}", esModule = embed "${module.esModule}")`;
 
 const moduleListCapnpify = (moduleList: ModuleList): string => `modules = [${moduleList.map(moduleCapnpify).join('')}]`;
 
@@ -34,16 +35,16 @@ const workerdCapnpify = (workers: WorkerdModule[]): CapnpWorkerNearName[] =>
 	}) satisfies CapnpWorkerNearName[];
 
 const createService = (workerCapnp: CapnpWorkerNearName): string =>
-	`(name="${workerCapnp.name}", worker=.${workerCapnp.name}),`;
+	`(name = "${workerCapnp.name}", worker = .${workerCapnp.name}),`;
 
 const createWorker = (name: string, worker: WorkerdModule): string =>
-	`const ${name} :Workerd.Worker = (${moduleListCapnpify(worker.modules)},compatibilityDate = ${worker.compatibilityDate});`;
+	`const ${name} :Workerd.Worker = (${moduleListCapnpify(worker.modules)},compatibilityDate = "${worker.compatibilityDate}");`;
 
 const servicesCapnpify = (capnpWorkerNearName: CapnpWorkerNearName[]): string =>
 	`services = [${capnpWorkerNearName.map(createService).join('')}],`;
 
 const createSocket = (defaultPort: number, defaultIp: Ip) => (workerCapnp: CapnpWorkerNearName) =>
-	`(name="http",address="${defaultIp}:${defaultPort++}",http=(),service="${workerCapnp.name}")`;
+	`(name = "http",address = "${defaultIp}:${defaultPort++}",http = (),service = "${workerCapnp.name}")`;
 
 const socketsCapnpify = (capnpWorkerNearName: CapnpWorkerNearName[], defaultPort: number, defaultIp: Ip): string =>
 	`sockets = [${capnpWorkerNearName.map(createSocket(defaultPort, defaultIp)).join('')}]`;
@@ -51,7 +52,7 @@ const socketsCapnpify = (capnpWorkerNearName: CapnpWorkerNearName[], defaultPort
 const configCapnpify = (workerCapnpNearNames: CapnpWorkerNearName[], defaultPort: number, defaultIp: Ip) =>
 	`const config :Workerd.Config = (${servicesCapnpify(workerCapnpNearNames)}${socketsCapnpify(workerCapnpNearNames, defaultPort, defaultIp)});`;
 
-const main = (defaultPort: number, defaultIp: Ip, filenames: string[]) => {
+const capnpGenerator = (defaultPort: number, defaultIp: Ip, filenames: string[]) => {
 	const workers = filenames.map((file: string) => workerdModuleFactory('worker', file, '2023-02-28'));
 
 	const workerCapnpNearNames = workerdCapnpify(workers);
@@ -63,4 +64,12 @@ const main = (defaultPort: number, defaultIp: Ip, filenames: string[]) => {
 	return finalCapnp;
 };
 
-console.log(main(8080, '*', ['index1.js', 'index2.js', 'index3.js']));
+const filenames = Array.from({length: 1000}, () => 'build/index.js');
+
+writeFile(
+	'my-config.capnp',
+	capnpGenerator(8080, '*', filenames),
+	err => {
+		console.log(err);
+	});
+
