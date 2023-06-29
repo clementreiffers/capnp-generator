848af9bf094df3026a4a43b52642c578a3e5e5bd
clementreiffers <reiffersclement@gmail.com>
2023-06-29 17:21:20 +0200

    capnp write in the current dir

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import {getAllFilesPathsFromBucket, createListFiles} from './s3-bucket';
 import {andThen, pipeWith, prop} from 'ramda';
 import generator from './generator';
-import {createFileInS3} from './upload';
+// Import {createFileInS3} from './upload';
 import {bucketName, fileName, s3Endpoint} from './constants';
+import * as fs from 'fs';
 
-const proceeedGenerationAndUploadCapnp = pipeWith(andThen)([
-	getAllFilesPathsFromBucket(bucketName, s3Endpoint),
-	prop('Contents'),
-	createListFiles,
-	generator(8080, '*'),
-	createFileInS3(bucketName, fileName),
-]);
+const writeFile = (path: string) => (content: string): void => {
+	fs.writeFile(path, content, err => {
+		if (err) {
+			console.log('error while trying to write capnp');
+		} else {
+			console.log('capnp writting successful');
+		}
+	});
+};
 
-const process = proceeedGenerationAndUploadCapnp();
+const generateCapnp = (): void => {
+	pipeWith(andThen)([
+		getAllFilesPathsFromBucket(bucketName, s3Endpoint),
+		prop('Contents'),
+		createListFiles,
+		generator(8080, '*'),
+		writeFile('./'),
+	// CreateFileInS3(bucketName, fileName),
+	])();
+};
+
+generateCapnp();
 
